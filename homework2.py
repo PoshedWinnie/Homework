5e8a2f9a4cd8cd79bcaf33e476c00dad4ff837a2
PoshedWinnie <poshedbver@gmail.com>
2024-07-08 15:49:58 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,11 @@
+first_name= 'Vasya'
+apple_count= 10
+second_name= 'Petya'
+orange_count= 15
+print(first_name,'дал',second_name,apple_count,'яблок и',orange_count,'апельсинов')
+
+amount_of_homework= 12
+amount_of_time_spent= 1.5
+name_of_course= 'Python'
+time_per_task= amount_of_homework/amount_of_time_spent
+print(name_of_course,amount_of_homework,amount_of_time_spent,time_per_task)
