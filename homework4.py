ec2be2118ece00bd618ebfd0f34d95d5a91832f1
PoshedWinnie <poshedbver@gmail.com>
2024-07-15 16:52:51 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,10 @@
+#my_string
+my_string= input("What is your name?")
+print("Hi,", my_string)
+print("You suck!")
+
+print("You suck!".upper())
+print("You suck!".lower())
+print("You suck!".replace(" ",""))
+print("You suck!"[0])
+print("You suck!"[8])
